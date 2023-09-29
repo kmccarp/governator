94ab137d598c48609610508e0d3fb5a3be0ff8ca
Kevin Carpenter™️ <kevin@moderne.io>
2023-09-29 19:23:06 +0000

    refactor: Common static analysis issues
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.staticanalysis.CommonStaticAnalysis?organizationId=RGVmYXVsdA%3D%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- governator-legacy/src/main/java/com/netflix/governator/guice/bootstrap/GovernatorBootstrap.java
+++ governator-legacy/src/main/java/com/netflix/governator/guice/bootstrap/GovernatorBootstrap.java
@@ -22,8 +22,9 @@ public class GovernatorBootstrap implements BootstrapModule {
     
     @Override
     public void configure(BootstrapBinder binder) {
-        if (config.enableAutoBindSingleton() == false)
+        if (config.enableAutoBindSingleton() == false) {
             binder.disableAutoBinding();
+        }
         binder.inStage(config.stage());
         binder.inMode(config.mode());
         
